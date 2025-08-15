a2425669d148ebdfc137d4e2cf66f643f18bc4f0
Keternooo <keterno@proton.me>
2025-08-15 08:59:34 +0200

    Fix …

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-subfinder -silent -d $1
-subfinder -silent -d "$1" -nW -oI | awk -F',' '{ if (NF >= 2) { print $1","$2 } else { print } }' > "$1.txt"
+subfinder -silent -d $1 -o $1.txt -nW -oI
