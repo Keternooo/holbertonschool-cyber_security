98caaecc76b7c6af2cc814e3b98d59a3da901800
xfc <keterno@proton.me>
2025-09-15 10:25:06 +0200

    Automatic push

---
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function ord {
+        printf %d "'$1";
+}
+input="$1"
+
+if [[ "$input" == {xor}* ]]; then
+    input="${input:5}"
+fi
+
+e=`echo "$input" | base64 --decode`
+seq 0 $((${#e}-1)) | while read line
+do
+        #echo ${e:$line:1};done
+        char=$((`ord ${e:$line:1}` ^ `ord _`))
+        printf \\$(printf '%03o' $char)
+done
