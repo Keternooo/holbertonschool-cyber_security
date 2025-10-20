39adc972e12d99992534dbcc39190b87bef9ac7e
xfc <keterno@proton.me>
2025-10-20 13:11:18 +0200

    Automatic push

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=~/Downloads/rockyou.txt --format=lm "$1" && john --show --format=lm "$1" | grep ':' | cut -d: -f2- > 4-password.txt
-
+john --wordlist=~/Downloads/rockyou.txt --format=raw-md5 "$1" && john --show --format=raw-md5 "$1" | grep ':' | cut -d: -f2- > 4-password.txt
--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=~/Downloads/rockyou.txt --format=nt "$1" && john --show --format=nt "$1" 5-password.txt
+john --wordlist=~/Downloads/rockyou.txt --format=nt "$1" && john --show --format=nt "$1" | grep ':' | cut -d: -f2- > 5-password.txt
